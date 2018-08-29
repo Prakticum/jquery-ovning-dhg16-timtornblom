043f22dcde7b9fd8ff03b2b832ed50f5935681ea
timtornblom <tim.tornblomz@gmail.com>
2018-08-29 14:14:38 +0300

    uppg 6-7

--- jquery_ovn_1.js
+++ jquery_ovn_1.js
@@ -13,6 +13,18 @@ $("#uppg4-box").html("h: " + uppg4height + "px  w: " + uppg4width + "px");
 //uppg5
 $("#uppg5-box").show("slow");
 
+//uppg6
+$("#uppg6-btn").click(function(){
+$("#uppg6-box").addClass("uppg6class")
+	});
+	
+//uppg7
+$("#uppg7-btn").click(function(){
+	$("#uppg7-box").animate({
+		"width" : "+=100px",
+		"height" : "+=100px"
+	});
+});
 
 
 
